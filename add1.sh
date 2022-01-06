8b5b158005bd21197e233f107862d591ba3d9a4e
sukriti95 <sukriti852@gmail.com>
2022-01-06 22:52:51 +0530

    Thirdcommmit

---
+++ add1.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -x
+read -p "Enter first number : " x
+read -p "Enter Second  number:" y
+z=$(($x+$y))
+echo $z
+
---
+++ initialze_varible.sh
@@ -0,0 +1,13 @@
+#! /bin/bash/ -x
+
+#To initialize a variable
+echoValue="Hello World"
+
+echo $echoValue
+
+#for loop
+for((i=o;i<10;i++))
+do
+   #body of the for loop
+   echo  $i
+done
